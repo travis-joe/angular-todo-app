a42ef39561f302294b9ac4ef5e0661846028ffef
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-08-01 11:06:01 +0800

    Part 1 — Getting our first version of the Todo application up and running

---
+++ src/app/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from "@angular/core";
+import { TodoDataService } from "../todo-data.service";
+import { Todo } from "../todo";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [TodoDataService]
+})
+export class AppComponent {
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {}
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+}
