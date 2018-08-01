36a3e3af8e83833bdbbfae4d2338ce85e1dd2651
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-08-01 17:50:30 +0800

    Part 3 — Update the Todo service to communicate with a REST API back end

--- src/app/app/api.service.ts
+++ src/app/app/api.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { HttpClient } from '@angular/common/http';
+import {Todo} from '../todo';
+import {Observable} from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+const API_URL = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // API: GET /todos
+  public getAllTodos(): Observable<Todo[]> {
+    return this.http
+      .get(API_URL + '/todos')
+      .map(response => {
+        const todos = response.json();
+        return todos.map((todo) => new Todo(todo));
+      })
+      .catch(this.handleError);
+  }
+
+  // API: POST /todos
+  public createTodo(todo: Todo) {
+    return this.http
+      .post(API_URL + '/todos', todo)
+      .map(response => {
+        return new Todo(response.json());
+      })
+      .catch(this.handleError);
+  }
+
+  // API: GET /todos/:id
+  public getTodoById(todoId: number) {
+    return this.http
+      .get(API_URL + '/todos/' + todoId)
+      .map(response => {
+        return new Todo(response.json());
+      })
+      .catch(this.handleError);
+  }
+
+  // API: PUT /todos/:id
+  public updateTodo(todo: Todo) {
+    return this.http
+      .put(API_URL + '/todos/' + todo.id, todo)
+      .map(response => {
+        return new Todo(response.json());
+      })
+      .catch(this.handleError);
+  }
+
+  // DELETE /todos/:id
+  public deleteTodoById(todoId: number) {
+    return this.http
+      .delete(API_URL + '/todos/' + todoId)
+      .map(response => null)
+      .catch(this.handleError);
+  }
+
+  private handleError (error: Response | any) {
+    console.error('ApiService::handleError', error);
+    return Observable.throw(error);
+  }
 }
