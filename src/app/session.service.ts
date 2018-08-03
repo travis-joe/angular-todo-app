fc14f9f532d1811cb6bd16a65ab46b6b3834a7a7
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-08-03 17:09:52 +0800

    Part 5

---
+++ src/app/session.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SessionService {
+
+  public accessToken: string;
+  public name: string;
+
+  constructor() {
+  }
+
+  public destroy(): void {
+    this.accessToken = null;
+    this.name = null;
+  }
+
+}
